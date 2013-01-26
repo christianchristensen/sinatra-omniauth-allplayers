650346d154e9ac97d0a188ac25c2e3830d6ddd2a
Chris Christensen <christian.christensen@gmail.com>
2013-01-26 11:23:07 -0600

    Update sinatra app to omniauth-att and example path matching.

--- app.rb
+++ app.rb
@@ -1,21 +1,59 @@
-require 'omniauth-allplayers'
+require 'omniauth-att'
 
 class MySinatraApp < Sinatra::Base
+  configure do
+    set :sessions, true
+    set :inline_templates, true
+  end
   use Rack::Session::Cookie
   use OmniAuth::Builder do
-    provider :AllPlayers, 'consumerkey', 'consumersecret'
+    provider :att, 'a1236b269f7ae1a8482e78fb1e87cf4f', '06065e26e04cf44e'
   end
 
   get '/' do
-    <<-HTML
-    <a href='/auth/allplayers'>Sign in with AllPlayers</a>
+    erb <<-HTML
+    <a href='/auth/att'>Sign in with AT&T</a>
 
     HTML
   end
 
-  get '/auth/allplayers/callback' do
-    auth = request.env['omniauth.auth']
-    # do whatever you want with the information!
-    "Hello #{auth.info['nickname']}"
+  get '/auth/:provider/callback' do
+    erb "<h1>#{params[:provider]}</h1>
+         <pre>#{JSON.pretty_generate(request.env['omniauth.auth'])}</pre>"
+  end
+
+  get '/auth/failure' do
+    erb "<h1>Authentication Failed:</h1><h3>message:<h3> <pre>#{params}</pre>"
+  end
+
+  get '/auth/:provider/deauthorized' do
+    erb "#{params[:provider]} has deauthorized this app."
+  end
+
+  get '/protected' do
+    throw(:halt, [401, "Not authorized\n"]) unless session[:authenticated]
+    erb "<pre>#{request.env['omniauth.auth'].to_json}</pre><hr>
+         <a href='/logout'>Logout</a>"
+  end
+
+  get '/logout' do
+    session[:authenticated] = false
+    redirect '/'
   end
 end
+
+__END__
+
+@@ layout
+<html>
+  <head>
+    <link href='http://twitter.github.com/bootstrap/1.4.0/bootstrap.min.css' rel='stylesheet' />
+  </head>
+  <body>
+    <div class='container'>
+      <div class='content'>
+        <%= yield %>
+      </div>
+    </div>
+  </body>
+</html>
